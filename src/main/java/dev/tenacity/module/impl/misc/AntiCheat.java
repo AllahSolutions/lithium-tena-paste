c54d1021e1c0cfdf91c940f49aa9de44cde09ef9
Liticane <liticane@gmail.com>
2023-07-17 04:06:00 -0700

    Update AntiCheat.java

--- src/main/java/dev/tenacity/module/impl/misc/AntiCheat.java
+++ src/main/java/dev/tenacity/module/impl/misc/AntiCheat.java
@@ -1,7 +1,7 @@
 package dev.tenacity.module.impl.misc;
 
 import dev.tenacity.event.impl.game.TickEvent;
-import dev.tenacity.hackerdetector.DetectionManager;
+import dev.tenacity.anticheat.DetectionManager;
 import dev.tenacity.module.Category;
 import dev.tenacity.module.Module;
 import dev.tenacity.utils.player.ChatUtil;
